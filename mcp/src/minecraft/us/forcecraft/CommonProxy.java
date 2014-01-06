f9f4248aefcd5053c94dd09805c45924b1bfb4e9
Pat Patterson <ppatterson@salesforce.com>
2014-01-06 14:37:22 -0800

    Remove master

--- mcp/src/minecraft/us/forcecraft/CommonProxy.java
+++
@@ -1,9 +0,0 @@
-package us.forcecraft;
-
-public class CommonProxy {
-
-    // Client stuff
-    public void registerRenderers() {
-            // Nothing here as the server doesn't render graphics or entities!
-    }
-}
