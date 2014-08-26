9f25164e48c2e99635bba74269ca55383d0750f1
Pat Patterson <ppatterson@salesforce.com>
2014-08-25 21:40:37 -0700

    Fixing issues with build

---
+++ src/main/java/us/forcecraft/client/ClientProxy.java
@@ -0,0 +1,13 @@
+package us.forcecraft.client;
+
+import us.forcecraft.CommonProxy;
+import net.minecraftforge.client.MinecraftForgeClient;
+
+public class ClientProxy extends CommonProxy {
+
+	@Override
+	public void registerRenderers() {
+		// This is for rendering entities and so forth later on
+	}
+
+}
